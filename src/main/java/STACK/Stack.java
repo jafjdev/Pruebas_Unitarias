2f9aacd7f73bae56554ce35fb9145cb4f7523d7e
Unknown <jafjdev@gmail.com>
2018-11-06 22:19:44 +0100

    Se completo la clase stack
    lista la clase stack

--- src/main/java/STACK/Stack.java
+++ src/main/java/STACK/Stack.java
@@ -1,25 +1,34 @@
 package STACK;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Stack {
-    private int maxStack;
-    private int emptyStack;
-    private int top;
-    private char[] items;
+  @Getter @Setter private int maxStack;
+  @Getter @Setter private int emptyStack;
+  @Getter @Setter private int top;
+  @Getter @Setter private char[] items;
 
-    public Stack(int size) {
-        this.maxStack = size;
-        this.emptyStack = -1;
-        this.top = emptyStack;
-        this.items = new char[maxStack];
-    }
+  public Stack(int size) {
+    this.maxStack = size;
+    this.emptyStack = -1;
+    this.top = emptyStack;
+    this.items = new char[maxStack];
+  }
 
-    public void push(char c){
-        items[++top] = c;
-    }
+  public void push(char c) {
+    items[++top] = c;
+  }
 
-    public char pop(){
-        return items[top--];
-    }
+  public char pop() {
+    return items[top--];
+  }
+
+  public boolean full() {
+    return top + 1 == maxStack;
+  }
+
+  public boolean empty() {
+    return top == emptyStack;
+  }
 }
-
-
