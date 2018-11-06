c9a7392a642941e5426cc65e94fd5b835cab87dc
Unknown <jafjdev@gmail.com>
2018-11-06 22:42:51 +0100

    Se realizo el test correctamente de Stack

--- src/test/java/STACK_TEST/StackTest.java
+++ src/test/java/STACK_TEST/StackTest.java
@@ -1,16 +1,46 @@
 package STACK_TEST;
 
+import STACK.Stack;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StackTest {
+  Stack s;
 
-    @Test
-    void push() {
-    }
+  @BeforeEach
+  public void init() {
+    s = new Stack(3);
+    s.push('a');
+    s.push('b');
+  }
 
-    @Test
-    void pop() {
-    }
+  @Test
+  void push() {
+    s.push('z');
+    char[] c = s.getItems();
+    assertEquals('z', c[2]);
+  }
+
+  @Test
+  void pop() {
+    s.pop();
+    char[] c = s.getItems();
+    assertEquals('b', c[1]);
+  }
+
+  @Test
+  void full() {
+    s.push('x');
+    assertTrue(s.full());
+  }
+
+  @Test
+  void empty() {
+    s.pop();
+    s.pop();
+    assertTrue(s.empty());
+  }
 }
