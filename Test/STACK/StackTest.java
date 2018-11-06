fa7feaf99ab05774938f22a38cd112401faa55cf
Unknown <jafjdev@gmail.com>
2018-11-06 21:53:11 +0100

    Se cambio la ubicacion del test source root

--- Test/STACK/StackTest.java
+++
@@ -1,7 +0,0 @@
-package STACK;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class StackTest {
-
-}
---
+++ src/test/java/STACK_TEST/StackTest.java
@@ -0,0 +1,16 @@
+package STACK_TEST;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StackTest {
+
+    @Test
+    void push() {
+    }
+
+    @Test
+    void pop() {
+    }
+}
